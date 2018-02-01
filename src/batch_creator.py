ee7721c34df9e93b919ba461968965499098f491
Habibul Islam <habibulislam72@yahoo.com>
2018-02-01 03:37:01 +0200

    conditions for last batch has been fixed

--- src/batch_creator.py
+++ src/batch_creator.py
@@ -1,18 +1,23 @@
+"""
+Main function to form the batches
+"""
+
 import sys
 
 
-def get_batches(records):
+def make_batches(records):
     batch = []
+    temp_array = []
     for record in records:
-        temp_array = []
         if (sys.getsizeof(record)) >= 1000000:  # 1MB = 1000000 bytes
             continue
         else:
-            if len(temp_array) <= 5 and sys.getsizeof(temp_array) <= 5000000:
+            if len(temp_array) < 5 and sys.getsizeof(temp_array) <= 50:
                 temp_array.append(record)
             else:
                 batch.append(temp_array)
                 temp_array = []
                 temp_array.append(record)
 
+    batch.append(temp_array)
     return batch
