3243c13591732bf98f1ca53f92a284bfed956b3a
Habibul Islam <habibulislam72@yahoo.com>
2018-02-01 03:42:15 +0200

    functionality test passed

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
-#a = [int(x) for x in input().split()]
-import array_functions
+'''
+Main function to be executed in order to 
+take an array of records of variables size
+'''
+
+from batch_creator import make_batches
 
 
 def main():
-    records = []
-    while True:
-        entry = input('Enter your record (q to quit): ')
-        if entry.lower() == 'q':
-            break
-        records.append(entry)
-
-    if records:
-        print(records)
-
+    records = ['record1', 'record2', 'record3', 'record4', 'record5', 'record6',
+               'record7', 'record8', 'record9', 'record10', 'record11', 'record12', 'record13',
+               'record14', 'record15', 'record16', 'record17', 'record18', 'record19', 'record20']
+    print(make_batches(records))
 
 if __name__ == '__main__':
     main()
